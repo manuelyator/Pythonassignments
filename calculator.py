823bedc7713cf84125c78ba577cd86a688c51cda
manuelyator <115005556+manuelyator@users.noreply.github.com>
2023-09-28 22:17:39 +0300

    Add files via upload
    The calculator assignment

---
+++ calculator.py
@@ -0,0 +1,50 @@
+def greet():
+    print("Welcome to the Calculator!")
+    print("You can perform basic arithmetic operations.")
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero!"
+    return x / y
+
+while True:
+    greet()
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    if choice == '5':
+        print("Goodbye!")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print("Result: ", add(num1, num2))
+    elif choice == '2':
+        print("Result: ", subtract(num1, num2))
+    elif choice == '3':
+        print("Result: ", multiply(num1, num2))
+    elif choice == '4':
+        result = divide(num1, num2)
+        if isinstance(result, str):
+            print(result)
+        else:
+            print("Result: ", result)
+    else:
+        print("Invalid input. Please enter a valid choice.")
